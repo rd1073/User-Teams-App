28a211a88324c0586ed800e7a4e9d9adc617778f
rd1073 <rd1073@srmist.edu.in>
2023-12-03 16:03:25 +0530

    pagination buttons

--- client/src/components/Pagination.js
+++ client/src/components/Pagination.js
@@ -3,11 +3,26 @@ import "./Pagination.css";
 
 
 const Pagination = ({ page, pages, changePage }) => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
+    return (
+        pages > 1 && (
+          <div className="pagination">
+            <button
+              className="pagination__prev"
+              onClick={() => changePage((page) => page - 1)}
+              disabled={page === 1}
+            >
+              &#171;
+            </button>
+            {/*middlePagination*/}
+            <button
+              className="pagination__next"
+              onClick={() => changePage((page) => page + 1)}
+              disabled={page === pages}
+            >
+              &#187;
+            </button>
+          </div>
+        )
+      );
+    };
 export default Pagination
